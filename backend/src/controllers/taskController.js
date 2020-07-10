e69dde929b6c8de51b60f1a3e002bd6e40f218e0
Maiquel <maiquelp@gmail.com>
2020-07-10 00:30:13 -0300

    total and latest task

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -13,7 +13,34 @@ module.exports = {
             return res.status(204).send();
 
         } catch (err) {
-            return res.status(400).send('Registration failed' + err)
+            return res.status(400).send(`Registration failed. \n Original Message:\n ${err}`)
+        }
+    },
+
+    async indexTotal(req, res) {
+        const {from, to} = req.body;
+
+        try {
+            const taskTotal = await connection('task').sum('exp_time as total').whereBetween('submit', [from, to]);
+            //const taskSelect = await connection('task').sum('exp_time as total').whereBetween('submit', ['2020-07-10 00:00:00', '2020-07-10 23:59:59']);
+            
+            return res.json(taskTotal)
+
+        } catch (err) {
+            return res.status(400).send(`Request failed. \n Original Message:\n ${err}`)
+        }
+    },
+
+    async indexLatest(req, res) {
+        const {from, to} = req.body;
+
+        try {
+            const taskLatest = await connection('task').select('*').limit(5).orderBy('id', 'desc');
+            
+            return res.json(taskLatest)
+
+        } catch (err) {
+            return res.status(400).send(`Request failed. \n Original Message:\n ${err}`)
         }
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,4 +6,8 @@ const routes = express.Router();
 
 routes.post('/task', taskController.create);
 
+routes.get('/taskTotal', taskController.indexTotal);
+
+routes.get('/taskLatest', taskController.indexLatest);
+
 module.exports = routes;
