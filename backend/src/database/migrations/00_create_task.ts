8119ea142545583cbb08807e8b589fad69d33cac
Maiquel <maiquelp@gmail.com>
2020-11-11 23:26:35 -0300

    :bookmark: backend typescript and data mapper

--- backend/src/database/migrations/00_create_task.ts
+++ backend/src/database/migrations/00_create_task.ts
@@ -4,7 +4,6 @@ export async function up(knex: Knex) {
     return knex.schema.createTable('task', table => {
         table.increments('id').primary();
         table.decimal('time').notNullable();
-        table.decimal('exp_time');
         table.timestamp('submit').defaultTo(knex.fn.now());
     })
 };
